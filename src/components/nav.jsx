2218a4043f955064d9cd839493e6cfea8339057d
Frieeze <frieeze@users.noreply.github.com>
2023-09-29 16:20:38 +0200

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,23 +2,7 @@ const React = require('react');
 
 const Nav = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href='/'>Home</a>
-          <a href='/counter'>Counter</a>
-        </li>
-      </ul>
-
-      <style jsx>{`
-        nav a {
-          color: #white;
-          text-decoration: none;
-          font-size: 13px;
-          padding-right: 10px;
-        }
-      `}</style>
-    </nav>
+    <></>
   );
 };
 
